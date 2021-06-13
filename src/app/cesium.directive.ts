9d7ae92931a8d5b2a7fe622f48eeaa0f88f807d6
CharaLagou <lagou.chara@gmail.com>
2021-06-13 19:05:15 +0200

    Add simple czml

--- src/app/cesium.directive.ts
+++ src/app/cesium.directive.ts
@@ -1,5 +1,5 @@
 import { Directive, ElementRef, OnInit } from '@angular/core';
-import { Viewer } from 'cesium';
+import { Cartesian3, CzmlDataSource, Viewer } from 'cesium';
 
 @Directive({
   selector: '[appCesium]'
@@ -9,7 +9,9 @@ export class CesiumDirective implements OnInit {
   constructor(private el: ElementRef) {}
 
   ngOnInit(): void {
-    const viewer = new Viewer(this.el.nativeElement);
+    const viewer = new Viewer(this.el.nativeElement, { shouldAnimate: true });
+    viewer.dataSources.add( CzmlDataSource.load('https://raw.githubusercontent.com/CesiumGS/cesium/master/Apps/SampleData/simple.czml'));
+    viewer.camera.flyHome(0);
   }
 
 }
